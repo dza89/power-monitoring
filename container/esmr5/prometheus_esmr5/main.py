73076aa11f48310c4fbd9c0ecd6749da5bf7bdc9
Daan <daan@deloitte.nl>
2022-12-24 20:14:19 +0100

    go

--- container/esmr5/prometheus_esmr5/main.py
+++ container/esmr5/prometheus_esmr5/main.py
@@ -8,41 +8,44 @@ from smeterd.meter import SmartMeter
 
 
 # Definitions
-electricity_consumed_total = Gauge('electricity_consumed_total', 'Electricity consumption total year-to-date')
-electricity_consumed_low = Gauge('electricity_consumed_low', 'Electricity consumption tariff low year-to-date')
-electricity_consumed_high = Gauge('electricity_consumed_high', 'Electricity consumption tariff high year-to-date')
-electricity_produced_total = Gauge('electricity_produced_total', 'Electricity produced total year-to-date')
-electricity_produced_low = Gauge('electricity_produced_low', 'Electricity produced tariff low year-to-date')
-electricity_produced_high = Gauge('electricity_produced_high', 'Electricity produced tariff high year-to-date')
-gas_consumed_total = Gauge('p1_total_gas_used', 'Gas consumption total year-to-date')
+electricity_consumption = Gauge('electricity_consumption_current', 'Current electricity consumption in 1 Watt resolution')
+electricity_consumed_low = Gauge('electricity_consumed_low', 'Electricity consumed, tariff low, in 0,001 kWh')
+electricity_consumed_high = Gauge('electricity_consumed_high', 'Electricity consumed, tariff high, in 0,001 kWh')
+electricity_production = Gauge('electricity_production_current', 'Current electricity production in 1 Watt resolution')
+electricity_produced_low = Gauge('electricity_produced_low', 'Electricity produced, tariff low, in 0,001 kWh')
+electricity_produced_high = Gauge('electricity_produced_high', 'Electricity produced, tariff high, in 0,001 kWh')
+gas_consumption = Gauge('gas_consumption', 'Gas consumption in mˆ3')
+
+logging.debug('electricity_consumed_low: %s' 'test')
 
 def get_p1_metrics(p1_lines):
     # Convert to a list for simple parsing
     p1_list = str(p1_lines).splitlines()
 
     for p1_line in p1_list:
+        logging.debug("%s", p1_line)
         match p1_line:
-            case '1-0:1.8.1':
-                logging.debug("[{}] electricity_consumed_low: {}" .format(datetime.datetime.now(), markup_helper(p1_line)))
+            case '1-0:1.8.1.255':
+                logging.info("electricity_consumed_low: %s", markup_helper(p1_line))
                 electricity_consumed_low.set(markup_helper(p1_line))
-            case '1-0:1.8.2':
-                logging.debug("[{}] electricity_consumed_high: {}" .format(datetime.datetime.now(), markup_helper(p1_line)))
+            case '1-0:1.8.2.255':
+                logging.info("electricity_consumed_high: %s", markup_helper(p1_line))
                 electricity_consumed_high.set(markup_helper(p1_line))
-            case '1-0:2.8.1':
-                logging.debug("[{}] electricity_produced_low: {}" .format(datetime.datetime.now(), markup_helper(p1_line)))
+            case '1-0:2.8.1.255':
+                logging.info("electricity_produced_low: %s", markup_helper(p1_line))
                 electricity_produced_low.set(markup_helper(p1_line))
-            case '1-0:2.8.2':
-                logging.debug("[{}] electricity_produced_high: {}" .format(datetime.datetime.now(), markup_helper(p1_line)))
+            case '1-0:2.8.2.255':
+                logging.info("electricity_produced_high: %s", markup_helper(p1_line))
                 electricity_produced_high.set(markup_helper(p1_line))
-            case '1-0:1.7.0':    
-                logging.debug("[{}] electricity_consumed_total: {}" .format(datetime.datetime.now(), markup_helper(p1_line)))
-                electricity_consumed_total.set(markup_helper(p1_line))
-            case '1-0:2.7.0':   
-                logging.debug("[{}] electricity_produced_total: {}" .format(datetime.datetime.now(), markup_helper(p1_line)))
-                electricity_produced_total.set(markup_helper(p1_line))
-            case '0-1:24.2.1':      
-                logging.debug("[{}] gas_consumed_total: {}" .format(datetime.datetime.now(), markup_helper(p1_line)))
-                gas_consumed_total.set(markup_helper(p1_line))
+            case '1-0:1.7.0.255':    
+                logging.info("electricity_production: %s", markup_helper(p1_line))
+                electricity_production.set(markup_helper(p1_line))
+            case '1-0:2.7.0.255':   
+                logging.info("electricity_consumption: %s", markup_helper(p1_line))
+                electricity_consumption.set(markup_helper(p1_line))
+            case '0-1:24.2.1.255':      
+                logging.info("gas_consumption: %s", markup_helper(p1_line))
+                gas_consumption.set(markup_helper(p1_line))
 
 def markup_helper(str_line):
     '''
@@ -53,6 +56,7 @@ def markup_helper(str_line):
 
 if __name__ == '__main__':
     # Start up the server to expose the metrics.
+    logging.basicConfig(format='%(asctime)s %(message)s')
     start_http_server(8000)
     # Generate some requests.
     while True:
